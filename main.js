51f258a70892210d4684421d84b10f6e66fac442
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2022-10-21 17:47:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    recognition.start();
+} 
+
+
+camera = document.getElementById("Camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+ setTimeout(function(){
+    img_id = "lol1"
+    take_snapshot();
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+ },5000);
+ setTimeout(function(){
+    img_id = "lol2"
+    take_snapshot();
+    speak_data = "Taking your next Selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+ },10000);
+ setTimeout(function(){
+    img_id = "lol3"
+    take_snapshot();
+    speak_data = "Taking your next Selfie in 15 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+ },15000);
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+
+}
+function take_snapshot(){
+console.log(img_id);
+Webcam.snap(function(data_uri){
+if(img_id == "lol1"){
+    document.getElementById("result1").innerHTML = '<img id="result1"src="'+data_uri+'"></img>'
+}
+if(img_id == "lol2"){
+    document.getElementById("result2").innerHTML = '<img id="result2"src="'+data_uri+'"></img>'
+}
+if(img_id == "lol3"){
+    document.getElementById("result3").innerHTML = '<img id="result3"src="'+data_uri+'"></img>'
+}
+
+
+
+
+});
+
+}
